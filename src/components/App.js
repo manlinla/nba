6761a2394d7ef42d9ab8d049119490e1534812c0
manlinla <mld@berkeley.edu>
2018-05-21 17:51:09 -0700

    main skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import {TopNavBar} from "./TopNavBar"
+import {Main} from "./Main"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App"> /* App corresponding to .App at App.css, not the file name */
           <TopNavBar/>
-
-        <p className="App-intro">
-          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-        </p>
+          <Main/>
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Main extends Component {
+    render() {
+        return (
+            <div className="main">
+                <div className="profile">profile</div>
+                <div className="shotchart">shotchart</div>
+                <div className="clear"></div>
+            </div>
+        );
+    }
+}
